22ac619b7cf1f1dfff010eb3b01eb9141a24d2d4
Kirthika Sharma <140343023+skirthika05@users.noreply.github.com>
2025-08-28 02:37:16 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from "react-dom/client"
 import App from './App.jsx'
-import { UserContextProvider } from './Context/UserContext.jsx'
+import { UserContextProvider } from './context/UserContext.jsx'
 import { CourseContextProvider } from './context/CourseContext.jsx';
 
 export const server ='https://educore-server-db0y.onrender.com';
